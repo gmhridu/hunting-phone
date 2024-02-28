78a026edde5ea6b53444812664c5a1efd7d9e798
gmhridu <hasanmahabubhridoy9@gmail.com>
2024-02-28 13:11:16 +0600

    card section added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-alert('ok');
+const loadPhone = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/phones?search=iphone"
+  );
+  const data = await res.json();
+  const phones = data.data;
+  console.log(phones);
+};
+
+loadPhone();
